f785dde69d003ab3adf50a21c38087181d24dbb9
Jimmie Jephson <jimmie.jephson@gmail.com>
2024-01-07 23:24:16 +0100

    Started on the Team log page

--- pages/teamlog.vue
+++ pages/teamlog.vue
@@ -1,6 +1,10 @@
 <template>
 	<section>
 		<h2>Team log</h2>
+		<ul class="log">
+			<li>Item 1</li>
+			<li>Item 2</li>
+		</ul>
 	</section>
 </template>
 
@@ -9,3 +13,19 @@
 		layout: 'default'
 	});
 </script>
+
+<style scoped>
+	.log {
+		display: flex;
+		flex-direction: column;
+		border: 1px solid saddlebrown;
+	}
+	.log li {
+		flex-basis: 100%;
+		width:100%;
+		background-color:#F4EEEC;
+	}
+	.log li:nth-of-type(even) {
+		background-color: #E9DEDA;
+	}
+</style>
